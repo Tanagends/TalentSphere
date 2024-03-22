bbe6548672ae7ac1d1d93b474ac8d8fbc5279814
Eric <stephen.erich243@gmail.com>
2024-03-23 00:19:00 +0100

    updated

--- forms.py
+++ forms.py
@@ -2,39 +2,39 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class BaseForm(FlaskForm):
     """Base form inherited by all forms"""
 
-    name = StringField('Name', validators=
-                       [DataRequired(message="Please enter your name"),
-                       Length(min=2, max=36))
+    name = StringField('Name', validators=[DataRequired(message="Please enter your name"),
+                       Length(min=2, max=36)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone_number = StringField('Mobile number', validators=[DataRequired(),
                                Length(min=5, max=15)])
-    city = StringField('City', validators=[DataRequired(), Length(min=2, max=36)])
-    country = StringField('Postal code', validators=[DataRequired(), Length(min=2, max=36])
+    city = StringField('City', validators=[
+                       DataRequired(), Length(min=2, max=36)])
+    country = StringField('Postal code', validators=[
+                          DataRequired(), Length(min=2, max=36)])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators[DataRequired(),
-                                     EqualTo('password', message='Passwords must match.')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
+                                                                     EqualTo('password', message='Passwords must match.')])
     submit = SubmitField('Register')
 
 
 class ScoutPlayerForm(BaseForm):
     """Player and scout base registration form"""
 
-    surname = StringField('Surname', validators=
-                       [DataRequired(message="Please enter your surname"),
-                       Length(min=2, max=36))
+    surname = StringField('Surname', validators=[DataRequired(message="Please enter your surname"),
+                                                 Length(min=2, max=36)])
     DOB = StringField('Date of Birth', validators=[DataRequired()])
 
 
 class PlayerForm(ScoutPlayerForm):
     """Player registration form"""
 
-    position = StringField('Position', validators=[Datarequired(),
+    position = StringField('Position', validators=[DataRequired(),
                            Length(min=2, max=36)])
     club = StringField('Club', validators=[Length(min=2, max=36)])
     academy = StringField('Academy', validators=[Length(min=2, max=36)])
