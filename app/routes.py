ead73ce43255bd9c5d6adb78393f7471f139c3f5
Tanagends <tanagends@gmail.com>
2024-03-24 13:36:44 +0200

    all

--- app/routes.py
+++ app/routes.py
@@ -20,19 +20,41 @@ def index():
     """Landing page"""
     return f"Hello talentsphere"
 
+@app.route('/signup', methods=["GET", "POST"])
+def sign_up():
+    """The signup page to get the user role"""
+
+    if request.method == "GET":
+        return render_template("signup.html")
+    
+    field = request.form['role']
+
+    if field == 'Football player':
+        return redirect(url_for('/signup/player'))
+    elif field == 'Scout':
+        return redirect(url_for('scout_form'))
+    elif field == 'Football club':
+        return redirect(url_for('club_form'))
+    elif field == 'Football Academy':
+        return redirect(url_for('football_form'))
+    elif field == 'Sponsor':
+        return redirect(url_for('sponsor_form'))
+    else:
+        flash("Invalid option")
+    
+
+
 
 @app.route('/signup/player', methods=['GET', 'POST'])
 def player_form():
     """This function handle the logic for the player form"""
-    
+ 
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    
+
     player = PlayerForm()
     if request.method == 'POST':
-        role = request.form['role']
-        process_result = process(role)
-        
+
         if process_result:
             if player.validate_on_submit():
                 players = {
