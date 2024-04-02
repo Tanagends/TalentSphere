fc2d944f502360fd193b18bf2c9faa2e998d8e77
Tanagends <tanagends@gmail.com>
2024-04-02 16:34:32 +0200

    all

--- app/new_routes.py
+++ app/new_routes.py
@@ -1,1 +1,27 @@
-from flask import
+"""MY application"""
+from flask import render_template, redirect, url_for, flash, Blueprint
+from flask_login import current_user, login_user, logout_user, login_required
+from flask import request
+from app.forms import BaseForm
+from app.forms import ScoutPlayerForm
+from app.forms import PlayerForm
+from app.forms import ScoutForm
+from app.forms import ClubForm
+from app.forms import AcademyForm
+from app.forms import SponsorForm, LoginForm
+from app.models.basemodel import BaseModel
+from app.models.player import Player
+from app.models.scout import Scout
+from app.models.club import Club
+from app.models.academy import Academy
+from app.models.sponsor import Sponsor
+from app.process import base_fields, user_signup_helper
+from app import db
+from app import login_manager
+from app.routes import main_app
+
+@main_app.route('/profile/<str:id>')
+def profile(id):
+    """Displays the profile of the player"""
+
+    player 
