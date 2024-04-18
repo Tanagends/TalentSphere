33908fc3b62250af7c8b95ac2ad7a56da7068d2a
Tanagends <tanagends@gmail.com>
2024-04-18 02:17:04 +0200

    all

--- app/new_routes.py
+++ app/new_routes.py
@@ -50,7 +50,7 @@ def profiles():
     #excluded_fields = ["password", "email"]
 
     if request.method == "GET":
-        players = Player.query.order_by(db.func.random()).limit(10).all()
+        players = Player.query.order_by(db.func.random()).limit(12).all()
         """
         dict_list = []
         for player in players:
@@ -80,21 +80,31 @@ def profiles():
             min_age, max_age = 0, 15
         elif age == "U18":
             min_age, max_age = 15, 18
+        elif age == "U21":
+            min_age, max_age = 18, 21
         elif age == "U23":
             min_age, max_age = 18, 23
         elif age == "Above 23":
-            min_age, max_age = 23, 50
+            min_age, max_age = 23, 500
         else:
             min_age, max_age = None, None
 
+        """
         age_expr = (db.extract('year', db.func.now()) - db.extract('year', Player.DOB)) -\
                     ((db.extract('month', db.func.now), db.extract('day', db.func.now()))\
                     < (db.extract('month', Player.DOB), db.extract('day', Player.DOB)))
+        """
 
         if min_age and max_age:
-            query = query.filter(and_(age_expr >= min_age, age_expr < max_age))
-
-        players = query.limit(12).all()
+            plyrs = query.all()
+            players = []
+            for player in plyrs:
+                if len(players) < 12 and min_age <= player.age < max_age:
+                    print(player.age)
+                    players.append(player)
+            #query = query.filter(and_(age_expr >= min_age, age_expr < max_age))
+        else:    
+            players = query.limit(12).all()
 
         return render_template('profiles.html', players=players)
   
